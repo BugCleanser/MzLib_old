043e4855600373ac5d11bd23b16a8226f8701d33
lvyitian <535481388@qq.com>
2023-05-20 09:55:21 +0800

    fix compatibility with 1.19.4

--- MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/wrappednms/NmsPlayerConnection.java
+++ MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/wrappednms/NmsPlayerConnection.java
@@ -10,6 +10,6 @@ public interface NmsPlayerConnection extends NmsPacketListener
 	@WrappedBukkitFieldAccessor({@VersionName("networkManager"),@VersionName(minVer=17, value="a",maxVer=19),@VersionName(value="b",minVer=19)})
 	NmsNetworkManager getNetworkManager();
 	
-	@WrappedBukkitFieldAccessor({@VersionName("player"),@VersionName(minVer=17, value="b",maxVer=19),@VersionName(value="c",minVer=19)})
+	@WrappedBukkitFieldAccessor({@VersionName("player"),@VersionName(minVer=17, value="b",maxVer=19),@VersionName(value="c",minVer=19,maxVer=19.4f),@VersionName(value="@0",minVer=19.4f)})
 	NmsEntityPlayer getPlayer();
 }
