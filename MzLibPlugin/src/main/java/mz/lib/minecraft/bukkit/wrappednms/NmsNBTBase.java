77f0067da7a23f3be7d666b01c5b28fe76463f35
lvyitian <535481388@qq.com>
2023-06-01 10:55:43 +0800

    Update NmsNBTBase.java

--- MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/wrappednms/NmsNBTBase.java
+++ MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/wrappednms/NmsNBTBase.java
@@ -12,7 +12,7 @@ import java.util.List;
 @WrappedBukkitClass({@VersionName(value="nms.NBTBase",maxVer=17),@VersionName(value="net.minecraft.nbt.NBTBase",minVer=17)})
 public interface NmsNBTBase extends WrappedBukkitObject
 {
-	List<Class<? extends NmsNBTBase>> NBTWrappers=Lists.newArrayList(NmsNBTTagByte.class, NmsNBTTagCompound.class, NmsNBTTagInt.class, NmsNBTTagList.class, NmsNBTTagLong.class, NmsNBTTagShort.class, NmsNBTTagDouble.class, NmsNBTTagString.class);
+	List<Class<? extends NmsNBTBase>> NBTWrappers=Lists.newArrayList(NmsNBTTagByte.class, NmsNBTTagCompound.class, NmsNBTTagInt.class, NmsNBTTagList.class, NmsNBTTagLong.class, NmsNBTTagShort.class, NmsNBTTagFloat.class, NmsNBTTagDouble.class, NmsNBTTagString.class);
 	static NmsNBTBase wrap(Object nmsNbt)
 	{
 		for(Class<? extends NmsNBTBase> w: NBTWrappers)
